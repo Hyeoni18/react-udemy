a90dcd98eb5c2b21c1284ab5b7a580c212ae139c
hyeoni <gusgnl105@gmail.com>
2025-03-07 16:21:19 +0900

    [course] JS Refresher - function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,16 @@
-import { apiKey, abc as content } from "./util.js";
-// import 하려는 파일 내 모든 대상을 객체화 후 사용, default 값은 util.default
-import * as util from "./util.js";
+let userMessage = 'Hello world';
 
-console.log(util.apiKey);
-console.log(apiKey);
-console.log(content);
+console.log(userMessage);
+
+// message 는 default 값으로 greeting1 처럼 매개변수 전달하지 않을 경우 표출되는 값이다.
+function createGreeting(userName, message = "Hello!") {
+    console.log(userName);
+    console.log(message);
+  return "Hi, I am " + userName + ". " + message;
+}
+
+const greeting1 = createGreeting("Max");
+console.log(greeting1);
+
+const greeting2 = createGreeting("Manuel", "Hello, what's up?");
+console.log(greeting2);
--- assets/scripts/util.js
+++
@@ -1,5 +0,0 @@
-// 파일 별로 하나만 만들 수 있는 default, import { apiKey } 가 아닌 이름 지정 필요 -> import key from
-// 향후 react 파일은 이 방식을 많이 사용하니 알아두기, 파일에서 export 할 대상이 하나의 컴포넌트 뿐이기에.
-export default "adnasdoasflak1";
-export let apiKey = "adnasdoasflak1";
-export let abc = "abc";
