601841d6a63b40776c1eabd96407030a6e313761
hyeoni <gusgnl105@gmail.com>
2025-03-24 15:01:53 +0900

    [course] React Essentials - Props 대체 문법 (코드 간략하게 표현하기)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -11,20 +11,6 @@ function Header() {
     const description =  reactDescriptions[genRandomIt(2)];
     return (
         <header>
-            {/* Type {} is not assignable to type string | undefined,
-            TypeScript 사용하는 React 프로젝트에서 발생하는 타입 에러.
-            오류 메세지는 img 태그의 src 속성이 문자열이나 undefined 타입만
-            받을 수 있는데, 객체 타입을 할당하려고 시도하고 있다는 의미.
-            => 이미지 경로 변환을 위한 타입 선언 필요.
-            (만약 TypeScript가 이미지 import 를 처리하지 못한다면,
-            이미지에 대한 타입 선언이 필요할 수 있음.)
-            // 프로젝트 내 .d.ts 파일에 추가
-            declare module '*.png' {
-              const value: string;
-              export default value;
-            }
-            이 오류는 주로 ts와 이미지 로더 설정의 불일치로 인해 발생.
-            */}
             <img src={reactImg} alt="Stylized atom"/>
             <h1>React Essentials</h1>
             <p>
@@ -35,12 +21,12 @@ function Header() {
     );
 }
 
-function CoreConcept(props) {
+function CoreConcept({ image, title, description }) {
     return (
         <li>
-            <img src={props.image} alt={props.title} />
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
         </li>
     );
 }
@@ -52,12 +38,16 @@ function App() {
             <main>
                 <section id="core-concepts">
                     <h2>Core Concepts</h2>
-                    <CoreConcept
-                        title="Components"
-                        description="The core UI building block."
-                        image={componentsImg}
-                    />
-                    <CoreConcept title="Props" />
+                    <ul>
+                        <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                    </ul>
                 </section>
             </main>
         </div>
